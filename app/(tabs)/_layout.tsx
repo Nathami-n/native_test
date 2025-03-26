d224aad43fc9d4a38b72cc1fd79f8086a3536e60
Nathami-n <georgenathan010@gmail.com>
2025-03-26 21:48:50 +0300

    add: the bottom tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import Colors from '@/constants/Colors'
 import { Tabs } from 'expo-router'
 import React from 'react'
-import { FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons'
+import { FontAwesome5, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
 
 const TabsLayout = () => {
   return (
@@ -16,7 +16,27 @@ const TabsLayout = () => {
       <Tabs.Screen name="index" options={{
         tabBarLabel: "Explore",
         tabBarIcon: ({color, size}) => 
-          <FontAwesome5 name="search" color={color} size={size}/>
+          <Ionicons name="search" color={color} size={size}/>
+      }} />
+      <Tabs.Screen name="wishlists" options={{
+        tabBarLabel: "Wishlists",
+        tabBarIcon: ({color, size}) => 
+          <Ionicons name="heart-outline" color={color} size={size}/>
+      }} />
+      <Tabs.Screen name="trips" options={{
+        tabBarLabel: "Trips",
+        tabBarIcon: ({color, size}) => 
+          <FontAwesome5 name="airbnb" color={color} size={size}/>
+      }} />
+      <Tabs.Screen name="profile" options={{
+        tabBarLabel: "Profile",
+        tabBarIcon: ({color, size}) => 
+          <Ionicons name="person-circle" color={color} size={size}/>
+      }} />
+      <Tabs.Screen name="inbox" options={{
+        tabBarLabel: "Inbox",
+        tabBarIcon: ({color, size}) => 
+          <MaterialCommunityIcons name="message-outline" color={color} size={size}/>
       }} />
     </Tabs>
   )
