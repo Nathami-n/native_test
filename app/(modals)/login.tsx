5bc51985258e61c38a9b7ed7b84f87934ccd62b5
Nathami-n <georgenathan010@gmail.com>
2025-03-27 00:56:44 +0300

    outline button

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,53 +1,92 @@
-import Colors from '@/constants/Colors';
-import { defaultStyles } from '@/constants/styles';
-import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/styles";
+import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const LoginScreenModal = () => {
   useWarmUpBrowser();
   return (
     <View style={styles.container}>
-    <TextInput  autoCapitalize='none' placeholder='email' style={[defaultStyles.inputField, {marginBottom: 30}]}/>
+      <TextInput
+        autoCapitalize="none"
+        placeholder="email"
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
 
-    <TouchableOpacity style={defaultStyles.btn} >
-    <Text style={defaultStyles.btnText}>
-      Continue
-    </Text>
-    </TouchableOpacity>
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+      {/* or section */}
+      <View style={styles.separatorView}>
+        <View
+          style={{
+            borderBottomColor: "#000",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+            flex: 1,
+          }}
+        />
+        <Text style={styles.separator}>or</Text>
+        <View
+          style={{
+            borderBottomColor: "#000",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+            flex: 1,
+          }}
+        />
+      </View>
 
-    <View style={styles.separatorView} >
-    <View style={{
-      borderBottomColor: "#000",
-      borderBottomWidth: StyleSheet.hairlineWidth,
-      flex: 1
-    }}/>
-    <Text style={styles.separator}>or</Text>
-    <View style={{
-      borderBottomColor: "#000",
-      borderBottomWidth: StyleSheet.hairlineWidth,
-      flex: 1
-    }}/>
+      {/* outline buttons */}
+
+      <View>
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons  name="call-outline" size={24} style={defaultStyles.btnIcon}/>
+          <Text style={styles.btnOutlineText}>
+            Continue with Phone
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
-    </View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 26,
   },
   separatorView: {
     flexDirection: "row",
     alignItems: "center",
     gap: 10,
-    marginVertical: 30
+    marginVertical: 30,
   },
   separator: {
     fontFamily: "mon-sb",
-    color: Colors.grey
+    color: Colors.grey,
+  },
+  btnOutline: {
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    height: 50,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: "#000",
+    fontSize: 16,
+    fontFamily: "mono-sb"
   }
-})
-export default LoginScreenModal
+});
+export default LoginScreenModal;
