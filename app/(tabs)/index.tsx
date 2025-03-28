0ba4041401cc54d5c1d61f37f32345daf8491176
Nathami-n <georgenathan010@gmail.com>
2025-03-28 10:02:50 +0300

    add: the iamges

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,23 +4,26 @@ import { Stack } from 'expo-router'
 import React, { useMemo, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import listingsData from "@/assets/data/airbnb-listings.json";
+import { View } from 'react-native'
 
 const IndexTab = () => {
   const [category, setCategory] = useState("Tiny homes");
 
   const items = useMemo(() => listingsData as any, []);
-  
+
   const onDataChanged = (category: string) => {
     setCategory(category);
   }
   return (
-    <SafeAreaView style={{flex: 1, marginTop: 80}}>
+    <SafeAreaView style={{flex: 1, backgroundColor: "#fff" }}>
       <Stack.Screen  options={{
         header: () => {
           return <ExploreHeader onCategoryChanged={onDataChanged}/>
         }
       }}/>
-      <Listings listings={[]} category={category}/>
+     <View style={{marginTop: 15, flex:1}}>
+     <Listings listings={items} category={category}/>
+     </View>
     </SafeAreaView>
   )
 }
--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,17 +1,67 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import {
+  View,
+  Text,
+  FlatList,
+  ListRenderItem,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { defaultStyles } from "@/constants/styles";
+import { Link } from "expo-router";
 
 interface Props {
   listings: any[];
   category: string;
 }
-const Listings = ({listings, category}: Props) => {
-  return (
-    <View>
-      <Text>Listings</Text>
-    </View>
-  )
-}
+const Listings = ({ listings, category }: Props) => {
+  const [loading, setLoading] = useState(false);
 
-export default Listings
+  const listRef = useRef<FlatList>(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
+  }, [category]);
+  const renderRow: ListRenderItem<any> = ({ item }) => {
+    return (
+      <Link href={`/listing/${item.id}`} asChild>
+        <TouchableOpacity>
+          <View style={styles.listing}>
+            <Image
+              source={{
+                uri: item.medium_url,
+              }}
+              style={styles.image}
+            />
+          </View>
+        </TouchableOpacity>
+      </Link>
+    );
+  };
+
+  return (
+    <View style={defaultStyles.container}>
+      <FlatList
+        ref={listRef}
+        data={loading ? [] : listings}
+        renderItem={renderRow}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  listing: {
+    padding: 16,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+  },
+});
+
+export default Listings;
