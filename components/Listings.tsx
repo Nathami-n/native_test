6faa7ffc856bd4678f37449de84937c9ac6bac2a
Nathami-n <georgenathan010@gmail.com>
2025-03-28 10:12:53 +0300

    add: the animaatios

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -10,6 +10,7 @@ import {
 import React, { useEffect, useRef, useState } from "react";
 import { defaultStyles } from "@/constants/styles";
 import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 interface Props {
   listings: any[];
@@ -37,6 +38,31 @@ const Listings = ({ listings, category }: Props) => {
               }}
               style={styles.image}
             />
+            <TouchableOpacity style={{position: "absolute", top: 30, right: 30}}>
+              <Ionicons  name="heart-outline"
+              size={24} color={"#000"}
+              />
+              </TouchableOpacity>
+              {/* text */}
+              <View style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                gap: 4
+                
+              }}>
+                <Text style={{fontSize: 16, fontFamily: "mon-sb"}}>{item.name}</Text>
+                <View style={{flexDirection: "row"}}>
+                  <Ionicons name="star" size={16} color={"#000"} />
+                  <Text style={{fontFamily: "mon-sb"}}>{item.review_scores_rating /20}</Text>
+                </View>
+              </View>
+              <Text style={{fontFamily: "mon"}}>{item.room_type}</Text>
+
+              <View style={{flexDirection: "row", gap: 4}}>
+                <Text style={{fontFamily: "mon-sb"}}>${item.price}</Text>
+                <Text style={{fontFamily: "mon"}}>/night</Text>
+
+              </View>
           </View>
         </TouchableOpacity>
       </Link>
@@ -57,10 +83,13 @@ const Listings = ({ listings, category }: Props) => {
 const styles = StyleSheet.create({
   listing: {
     padding: 16,
+    gap: 10,
+    marginVertical: 12
   },
   image: {
     width: "100%",
     height: 300,
+    borderRadius: 10
   },
 });
 
